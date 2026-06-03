a4615358826b4722382d0a4bd6f4a66c11d61795
samuelalborghetti <alborghetti.samuel.09@itisdalmine.edu.it>
2026-06-03 10:45:47 +0200

    FASE 1: sw.js - Service Worker for PWA

---
+++ sw.js
@@ -0,0 +1,80 @@
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `calisthenics-tracker-${CACHE_VERSION}`;
+
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/variables.css',
+  './css/base.css',
+  './css/layout.css',
+  './css/components.css',
+  './css/themes.css',
+  './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching assets');
+      return cache.addAll(URLS_TO_CACHE).catch((error) => {
+        console.log('[Service Worker] Some assets failed to cache:', error);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[Service Worker] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match('./index.html');
+        });
+    })
+  );
+});
